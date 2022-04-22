e39f25dfa7c6c3e716a8df9859af8ac0713a5243
CaioCesarLima <cesarlimacaio@gmail.com>
2022-04-22 16:29:29 -0300

    fizzbuzz update test

--- fizzbuzz/test/fizzbuzz_test.exs
+++ fizzbuzz/test/fizzbuzz_test.exs
@@ -2,7 +2,8 @@ defmodule FizzbuzzTest do
   use ExUnit.Case
   doctest Fizzbuzz
 
-  test "greets the world" do
-    assert Fizzbuzz.hello() == :world
+  test "sould replace numbers divisible by three for :fizz and numbers divisible by five for :buzz and divisible by both for :fizzbuzz" do
+    assert Fizzbuzz.build("../numbers.txt") == {:ok,
+      [1, 2, :fizz, 4, :buzz, :fizz, 7, 8, :fizz, :fizzbuzz, 26, :buzz, 152, :buzz]}
   end
 end
